76e75d23574e64d2a36db5278717f198d734b70c
Kalande_Kapaya <kjkreed@gmail.com>
2022-12-03 16:41:12 +0200

    Drag functionality Complete

---
+++ script.js
@@ -0,0 +1,32 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties){
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter',dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop',dragDrop)
+}
+
+function dragStart(){
+    this.itemFloat += ' hold'
+    setTimeout(()=>this.itemFloat = ' invisible',0)
+}
+function dragEnd(){
+    console.log("drag End");
+}
+function dragOver(){
+    console.log("drag Over");
+}
+function dragEnter(){
+    console.log("drag Enter");
+}
+function dragLeave(){
+    console.log("drag Leave");
+}
+function dragDrop(){
+    console.log("drag Drop");
+}
